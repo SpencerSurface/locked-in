de27eb65da2d87905e350c29d9d10af4f221e250
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-02-28 19:47:36 -0500

    add Stake and Vote models

---
+++ models/Stake.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Stake extends Model {}
+
+Stake.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        amount: {
+            type: DataType.DECIMAL,
+            allowNull: false
+        },
+        user_id: {
+            type: DataType.INTEGER,
+            allowNull: false,
+            references: {
+                model: "user",
+                key: "id"
+            }
+        },
+        bet_id: {
+            type: DataType.INTEGER,
+            allowNull: false,
+            references: {
+                model: "bet",
+                key: "id"
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "stake"
+    }
+);
+
+module.exports = Stake;
