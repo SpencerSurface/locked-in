d37503c6751afcbbc90d549574652e942568d98b
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-03-06 15:03:54 -0500

    add logout script

---
+++ public/js/logout.js
@@ -0,0 +1,15 @@
+const logoutHandler = async (event) => {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Logout failed.');
+    }
+
+};
+
+document.querySelector('#logout').addEventListener('click', logoutHandler);
