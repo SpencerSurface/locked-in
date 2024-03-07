46bd34b04788b307ef439ef23770d9357b0a1fb7
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-03-06 19:59:44 -0500

    add randomly stakes with random users and bets

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -7,26 +7,46 @@ const stakeData = require("./stakeData.json");
 const voteData = require("./voteData.json");
 
 const seedDatabase = async () => {
+    // Sync with the database
     await sequelize.sync({force: true});
+
+    // Create users from file
     const users = await User.bulkCreate(userData, {
         individualHooks: true,
-        returning: true,
+        returning: true
     });
 
+    // Create bets from file
     const bets = await Bet.bulkCreate(betData, {
         individualHooks: true,
-        returning: true,
+        returning: true
     });
 
-    const stakes = await Stake.bulkCreate(stakeData, {
-        individualHooks: true,
-        returning: true,
-    });
+    // Create random stakes that agree with the users and bets (2 stakes per bet)
+    const stakes = [];
+    for (let i = 0; i < bets.length; i++) {
+        let bet = bets[i];
+        let userIdList = users.map((user) => user.dataValues.id);
+        // If there's a winner, create a stake for them
+        if (bet.dataValues.winner) {
+            stakes.push(await Stake.create({amount: 100, user_id: bet.dataValues.winner, bet_id: bet.dataValues.id}));
+            userIdList.splice(userIdList.indexOf(bet.dataValues.winner), 1);
+        // Else, create a first stake for a random user
+        } else {
+            let randomIndex = Math.floor(Math.random() * userIdList.length);
+            let randomUser = userIdList.splice(randomIndex, 1)[0];
+            stakes.push(await Stake.create({amount: 100, user_id: randomUser, bet_id: bet.dataValues.id}));
+        }
+        // Create the second stake for a random user
+        let randomIndex = Math.floor(Math.random() * userIdList.length);
+        let randomUser = userIdList.splice(randomIndex, 1)[0];
+        stakes.push(await Stake.create({amount: 100, user_id: randomUser, bet_id: bet.dataValues.id}));
+    }
 
-    const votes = await Vote.bulkCreate(voteData, {
-        individualHooks: true,
-        returning: true,
-    });
+    // const votes = await Vote.bulkCreate(voteData, {
+    //     individualHooks: true,
+    //     returning: true,
+    // });
 
     process.exit(0);
 }
