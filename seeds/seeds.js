573ae5958703c1e3018b497016f71a4dfd395be9
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-03-06 20:05:02 -0500

    add random amounts to the randomly generated stakes

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -27,20 +27,22 @@ const seedDatabase = async () => {
     for (let i = 0; i < bets.length; i++) {
         let bet = bets[i];
         let userIdList = users.map((user) => user.dataValues.id);
+        // Get a random number between 20% of the bet amount and 80% of the bet amount
+        let portion = Math.floor(((Math.random() * 6 + 2) / 10) * bet.amount);
         // If there's a winner, create a stake for them
         if (bet.dataValues.winner) {
-            stakes.push(await Stake.create({amount: 100, user_id: bet.dataValues.winner, bet_id: bet.dataValues.id}));
+            stakes.push(await Stake.create({amount: portion, user_id: bet.dataValues.winner, bet_id: bet.dataValues.id}));
             userIdList.splice(userIdList.indexOf(bet.dataValues.winner), 1);
         // Else, create a first stake for a random user
         } else {
             let randomIndex = Math.floor(Math.random() * userIdList.length);
             let randomUser = userIdList.splice(randomIndex, 1)[0];
-            stakes.push(await Stake.create({amount: 100, user_id: randomUser, bet_id: bet.dataValues.id}));
+            stakes.push(await Stake.create({amount: portion, user_id: randomUser, bet_id: bet.dataValues.id}));
         }
         // Create the second stake for a random user
         let randomIndex = Math.floor(Math.random() * userIdList.length);
         let randomUser = userIdList.splice(randomIndex, 1)[0];
-        stakes.push(await Stake.create({amount: 100, user_id: randomUser, bet_id: bet.dataValues.id}));
+        stakes.push(await Stake.create({amount: (bet.amount - portion), user_id: randomUser, bet_id: bet.dataValues.id}));
     }
 
     // const votes = await Vote.bulkCreate(voteData, {
