29d2c616b67f1dbbdfede7bfcc07c909077bc840
noah-10 <noah.fryman.10@gmail.com>
2024-03-12 18:38:27 -0400

    Added a getting started section on homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -2,14 +2,7 @@ const newBetHandler = async (event) => {
     document.location.replace('/profile/new-bet');
 };
 
-const loginHandler = async (event) => {
-    document.location.replace('/login');
-};
-
-const signupHandler = async (event) => {
-    document.location.replace('/signup');
-};
-
-document.querySelector('#new-bet-button').addEventListener('click', newBetHandler);
-document.querySelector('#login-button').addEventListener('click', loginHandler);
-document.querySelector('#signup-button').addEventListener('click', signupHandler);
+const newBet = document.querySelectorAll('#new-bet-button');
+newBet.forEach((btn) => {
+    btn.addEventListener('click', newBetHandler);
+});
